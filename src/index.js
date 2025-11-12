13b2f73fad2ad495df256c50a6bea85c23e03d7c
NghiaTT200000 <ggbotcute07@gmail.com>
2025-11-12 07:00:53 +0000

    Remove old code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import RouteGraphics from "./RouteGraphics";
-import { SpriteRendererPlugin } from "./plugins/elements/SpriteRendererPlugin";
-import { TextRendererPlugin } from "./plugins/elements/TextRendererPlugin";
-import { TextRevealingRendererPlugin } from "./plugins/elements/TextRevealingRendererPlugin";
-import { ContainerRendererPlugin } from "./plugins/elements/ContainerRendererPlugin";
-import { KeyframeTransitionPlugin } from "./plugins/transitions/KeyframeTransitionPlugin";
-import { RectRendererPlugin } from "./plugins/elements/RectRendererPlugin";
-import { SliderRendererPlugin } from "./plugins/elements/SliderRendererPlugin";
-import { AudioPlugin } from "./plugins/elements/AudioPlugin";
 import { createAssetBufferManager } from "./utils.js";
 import { renderApp } from "./render/renderApp.js";
 import parse from "./parser/index.js";
@@ -18,14 +10,6 @@ import transitionElements from "./transition/index.js";
 export default RouteGraphics;
 
 export {
-  SpriteRendererPlugin,
-  TextRendererPlugin,
-  ContainerRendererPlugin,
-  TextRevealingRendererPlugin,
-  KeyframeTransitionPlugin,
-  RectRendererPlugin,
-  AudioPlugin,
-  SliderRendererPlugin,
   Application,
   Assets,
   AudioAsset,
