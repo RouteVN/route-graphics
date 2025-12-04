e2e5654ae64bc146b1290edf8f1e25eca86d90e1
NghiaTT200000 <ggbotcute07@gmail.com>
2025-12-04 15:54:21 +0000

    Include negative in the cals

--- src/plugins/elements/container/parseContainer.js
+++ src/plugins/elements/container/parseContainer.js
@@ -18,7 +18,7 @@ import { parseCommonObject } from "../util/parseCommonObject.js";
  * If direction is set and the width/height is set than the container will wrap the element based on the setted width/height
  */
 export const parseContainer = ({ state, parserPlugins = [] }) => {
-  const direction = state.direction ?? "";
+  const direction = state?.direction ?? "";
   const scroll = state.scroll ? true : false;
   const gap = state.gap || 0;
   const children = structuredClone(state.children || []);
@@ -34,6 +34,10 @@ export const parseContainer = ({ state, parserPlugins = [] }) => {
   let lastColWidth = 0;
   let currentRowWidth = 0;
   let currentColHeight = 0;
+  let maxX = -Infinity;
+  let maxY = -Infinity;
+  let minX = Infinity;
+  let minY = Infinity;
 
   for (let i = 0; i < children.length; i++) {
     const gapValue = i < children.length - 1 ? gap : 0;
@@ -100,19 +104,38 @@ export const parseContainer = ({ state, parserPlugins = [] }) => {
       containerWidth = Math.max(child.width + child.x, containerWidth);
       containerHeight = Math.max(currentY, containerHeight);
     } else {
-      containerWidth = Math.max(child.width + child.x, containerWidth);
-      containerHeight = Math.max(child.height + child.y, containerHeight);
+      console.log("child ",child)
+      maxX = Math.max(child.x + child.width, maxX);
+      maxY = Math.max(child.y + child.height, maxY);
+      minX = Math.min(child.x, minX);
+      minY = Math.min(child.y, minY);
     }
 
     parsedChildren.push(child);
   }
 
-  const containerAST = parseCommonObject({
-    ...state,
-    width: state.width ? state.width : containerWidth,
-    height: state.height ? state.height : containerHeight,
-  });
-
+  let containerAST;
+  if(direction !== ""){
+    containerAST = parseCommonObject({
+      ...state,
+      width: state.width ? state.width : containerWidth,
+      height: state.height ? state.height : containerHeight,
+    });
+  }
+  else{
+    const virtualWidth =(maxX === Infinity || minX === Infinity)? 0 : maxX - minX;
+    const virtualHeight =(maxY === Infinity || minY === Infinity)? 0 : maxY - minY;
+    containerAST = parseCommonObject({
+      ...state,
+      x: Math.max(minX, state.x),
+      y: Math.min(maxY, state.y),
+      width: virtualWidth,
+      height: virtualHeight,
+    });
+    containerAST.width = containerWidth;
+    containerAST.height = containerHeight;
+  }
+  
   return {
     ...containerAST,
     children: parsedChildren,
