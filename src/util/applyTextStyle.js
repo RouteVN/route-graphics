61daaa1fc0daaf509e33222a6770cf21d4dd71a8
NghiaTT200000 <ggbotcute07@gmail.com>
2025-12-10 09:42:27 +0000

    Update tests and applyTextStyle to have wordWrapWidth

--- src/util/applyTextStyle.js
+++ src/util/applyTextStyle.js
@@ -8,10 +8,11 @@ export default (element, style) => {
     align: style?.align ?? DEFAULT_TEXT_STYLE.align,
     lineHeight: style?.lineHeight ?? DEFAULT_TEXT_STYLE.lineHeight,
     wordWrap: style?.wordWrap ?? DEFAULT_TEXT_STYLE.wordWrap,
-    breakWords: style?.wordWrap ?? DEFAULT_TEXT_STYLE.breakWords,
+    breakWords: style?.breakWords ?? DEFAULT_TEXT_STYLE.breakWords,
     strokeColor: style?.strokeColor ?? DEFAULT_TEXT_STYLE.strokeColor,
     strokeWidth: style?.strokeWidth ?? DEFAULT_TEXT_STYLE.strokeWidth,
+    wordWrapWidth: style?.wordWrapWidth ?? DEFAULT_TEXT_STYLE.wordWrapWidth,
   };
-
+  if(element.label === "en-text-true" || element.label === "en-text-false") console.log(appliedStyle)
   element.style = appliedStyle;
 };
